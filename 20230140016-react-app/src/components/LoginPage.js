feca68b8f9f3b5fd575e6fe3ab9900b7b9902434
HafizZada16 <hafizzada16@gmail.com>
2025-11-15 09:33:49 +0700

    Commit2

--- 20230140016-react-app/src/components/LoginPage.js
+++ 20230140016-react-app/src/components/LoginPage.js
@@ -48,8 +48,7 @@ justify-center"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="mt-1 w-full px-3 py-2 border border-gray-300
-rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:borderblue-500"
+              className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:borderblue-500"
             />
           </div>
           <div>
@@ -65,14 +64,12 @@ rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:borderblue-500
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="mt-1 w-full px-3 py-2 border border-gray-300
-rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:borderblue-500"
+              className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:borderblue-500"
             />
           </div>
           <button
             type="submit"
-            className="w-full py-2 px-4 bg-blue-600 text-white font-semibold
-rounded-md shadow-sm hover:bg-blue-700"
+            className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-md shadow-sm hover:bg-blue-700"
           >
             Login
           </button>
